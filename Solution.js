19f8dbaa4c48f2de995a9fa846c20cb855051119
Eugenia Grieff <eugeniagrieff@gmail.com>
2015-05-20 21:13:00 +0200

    Update Solution.js

--- Solution.js
+++ Solution.js
@@ -4,16 +4,10 @@ for(var i=1; i<1000; i++) {
     if(i % 3 === 0) {
           multThree += i;
 	}
-	else {
-		// Do not load 
-	}
 }
 for(var j=1; j<1000; j++) {
     if(j % 5 === 0) {
         multFive +=j;
-    }
-    else {
-        //Do not load
     }
 }
 console.log(multThree + multFive);
